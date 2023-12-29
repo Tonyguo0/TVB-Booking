3b0a74244f70e896aec69e45ee7938b36eafef58
tonyguo0 <tonyguo998@gmail.com>
2023-12-29 20:24:18 +0800

    changed to a simple form frontend to get ready to test backend capabilities

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { Container, Row } from 'react-bootstrap'
+import './App.scss'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container className="text-center">
+        <form action='submit'>
+          <div className='row justify-content-center'>
+            <div className='col-sm-4'>
+              <label htmlFor='first_name'>First Name:</label>
+              <input
+                id='first_name'
+                type='text'
+              />
+            </div>
+          </div>
+
+          <div className='row justify-content-center'>
+            <div className='col-sm-4'>
+              <label htmlFor='last_name'>Last Name:</label>
+              <input
+                id='last_name'
+                type='text'
+              />
+            </div>
+          </div>
+          <div className='row justify-content-center'>
+            <div className='col-sm-4'>
+              <label htmlFor='email'>Email:</label>
+              <input
+                id='email'
+                type='text'
+              />
+            </div>
+          </div>
+          <div className='row justify-content-center'>
+            <div className='col-sm-4'>
+              <label htmlFor='phone_no'>Phone Number:</label>
+              <input
+                id='phone_no'
+                type='text'
+              />
+            </div>
+          </div>
+          <div className='row justify-content-center'>
+            <div className='col-sm-4'>
+              <button
+                className='btn btn-primary'
+                type='submit'>
+                Pay
+              </button>
+            </div>
+          </div>
+          <div className='row justify-content-center'>
+            <div className='col-sm-4'></div>
+          </div>
+        </form>
+      </Container>
+      <Row></Row>
     </>
   )
 }
