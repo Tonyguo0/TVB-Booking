8a38f5e711a9f5ee2bea7484b6b9a6424ce9a3fc
tonyguo0 <tonyguo998@gmail.com>
2024-01-03 19:09:11 +0800

    added google sheet api posting capability works when submitting form and it adds in the names, email and mobile number

--- TVB-booking-backend/src/index.ts
+++ TVB-booking-backend/src/index.ts
@@ -2,23 +2,27 @@ import { Elysia } from "elysia";
 import { cors } from "@elysiajs/cors";
 import { testAddSheet } from "./sheet";
 import { IPlayer } from "./model/player";
+const app = new Elysia();
 
-const app = new Elysia()
-    .use(
-        cors({
-            allowedHeaders: ["authorization", "Content-Type"], // you can change the headers
-            exposedHeaders: ["authorization"], // you can change the headers
-            origin: /^.*?localhost:5173$/,
-            methods: [`GET`, `PUT`, `POST`, `DELETE`],
-            preflight: true
-        })
-    )
-    .get("/", () => "Hello Elysia!!!")
-    .get("/hello", () => "Hello World!")
-    .post("/player", ({ body }: { body: IPlayer }) => {
-        testAddSheet([body.first_name, body.last_name, body.email, body.phone_no]);
-        console.log(body);
+app.use(
+    cors({
+        allowedHeaders: ["authorization", "Content-Type"], // you can change the headers
+        exposedHeaders: ["authorization"], // you can change the headers
+        origin: /^.*?localhost:5173$/,
+        methods: [`GET`, `PUT`, `POST`, `DELETE`],
+        preflight: true
     })
-    .listen(3000);
+);
 
-console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
+app.get("/", () => "Hello Elysia!!!");
+
+app.get("/hello", () => "Hello World!");
+
+app.post("/player", ({ body }: { body: IPlayer }) => {
+    testAddSheet([body.first_name, body.last_name, body.email, body.phone_no]);
+    console.log(body);
+});
+
+app.listen(3000, () => {
+    console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
+});
